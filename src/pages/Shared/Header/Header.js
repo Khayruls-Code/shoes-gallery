9bc8fe9e0976d08f8867632b96019e864ed4dc6a
Khayrul Islam <khayrulislam2525@gmail.com>
2021-11-10 11:12:37 +0600

    header and banner section complete

---
+++ src/pages/Shared/Header/Header.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+
+const Header = () => {
+  return (
+    <nav className="navbar navbar-expand-lg navbar-light bg-light fixed-top">
+      <div className="container">
+        <NavLink to='/home' className="navbar-brand uppercase font-bold" href="/">Shoe Gallery</NavLink>
+        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav ms-auto">
+            <li className="nav-item py-2 ml-4">
+              <NavLink to='home' className="nav-link font-medium" aria-current="page" href="/">Home</NavLink>
+            </li>
+            <li className="nav-item py-2 ml-4">
+              <NavLink to='home' className="nav-link font-medium" aria-current="page" href="/">Explore More</NavLink>
+            </li>
+            <li className="nav-item py-2 ml-4">
+              <NavLink to='home' className="nav-link font-medium" aria-current="page" href="/">Contact</NavLink>
+            </li>
+            <li className="nav-item py-2 ml-4">
+              <button className='px-4 rounded-md text-white font-semibold py-2 bg-primary_bg'>Login</button>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Header;
---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+    colors: {
+      primary_bg: "#c70039"
+    }
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
